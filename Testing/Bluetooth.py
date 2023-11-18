9a204fcc8b9cf71d64852eae56b0a6b2b5b8fb5a
Charlie Brunt <cwhbrunt@gmail.com>
2023-11-18 18:02:57 +0000

    test

--- Testing/Bluetooth.py
+++ Testing/Bluetooth.py
@@ -6,19 +6,24 @@ import struct
 UUID = 'ca73b3ba-39f6-4ab3-91ae-186dc9577d99'
 
 async def main(address):
-  async with BleakClient(address) as client:
-    if (not client.is_connected):
-      raise "client not connected"
+  async with BleakScanner() as scanner:
+      devices = await scanner.discover()
+      for d in devices:
+          print(d)
 
-    services = await client.get_services()
+  # async with BleakClient(address) as client:
+  #   if (not client.is_connected):
+  #     raise "client not connected"
 
-    received_data = await client.read_gatt_char(UUID)
-    # name = bytearray.decode(name_bytes)
+  #   services = await client.get_services()
 
-    # Convert the byte array to uint16
-    uint16_values = struct.unpack('<' + 'H' * (len(received_data) // 2), received_data)
-    print(received_data)
-    print(uint16_values)
+  #   received_data = await client.read_gatt_char(UUID)
+  #   # name = bytearray.decode(name_bytes)
+
+  #   # Convert the byte array to uint16
+  #   uint16_values = struct.unpack('<' + 'H' * (len(received_data) // 2), received_data)
+  #   print(received_data)
+  #   print(uint16_values)
 
 if __name__ == "__main__":
   address = "4B:63:04:4D:61:10"
