17dff04fdc3b0eb961d72dd7c19d4ff8b72f5786
Charlie-Brunt <cwhbrunt@gmail.com>
2024-02-20 10:19:45 +0000

    Serial

--- Testing/Receive.py
+++ Testing/Receive.py
@@ -1,35 +1,26 @@
 import serial
 import sys
 import platform
+import serial.tools.list_ports
+
 
 def find_arduino_port():
-    if platform.system() == 'Darwin':  # macOS
-        arduino_ports = ["/dev/tty.usbserial", "/dev/ttyUSB", "/dev/ttyACM"]
-    elif platform.system() == 'Windows':
-        arduino_ports = ["COM" + str(i + 1) for i in range(256)]
-    else:
-        print("Unsupported platform")
-        sys.exit(1)
-
-    for port in arduino_ports:
-        try:
-            ser = serial.Serial(port)
-            ser.close()
-            return port
-        except serial.SerialException:
-            pass
+    arduino_ports = list(serial.tools.list_ports.comports())
+    for p in arduino_ports:
+        if "XIAO" in p[1]:
+            return p[0]
     print("Couldn't find Arduino port.")
     sys.exit(1)
 
-def connect_to_arduino():
+def connect_to_arduino(baudrate):
     arduino_port = find_arduino_port()
     print("Connecting to Arduino on port:", arduino_port)
-    ser = serial.Serial(arduino_port, baudrate=9600, timeout=1)
+    ser = serial.Serial(arduino_port, baudrate=baudrate, timeout=1)
     return ser
 
 
 # Configure the serial port
-port = serial.Serial('COM6', 115200)  # Replace 'COM1' with the appropriate serial port
+port = connect_to_arduino(115200)
 
 # Create a buffer to store the received data
 buffer_size = 512
