b109f76cf726d453be960884a305e2e7e7fa302a
Charlie-Brunt <cwhbrunt@gmail.com>
2024-03-03 22:38:35 +0000

    update test data

--- Firmware/Test data.ino
+++ Firmware/Test data.ino
@@ -3,31 +3,35 @@
 #include <Adafruit_TinyUSB.h> // for Serial
 
 // LED
-const int ledPin = LED_BLUE;
+const int ledPin = LED_RED;
 int ledState = LOW;
 unsigned long previousMillis = 0;
 const long interval = 500;
 
 // Parameters
-const int frequency = 1;
-const int sampling_frequency = 200;
+const int frequency = 2;
+const int sampling_frequency = 250;
 const int bit_depth = 8;
 int levels = pow(2, bit_depth) - 1;
+int channels = 5;
 
 
 void setup() {
   pinMode(ledPin, OUTPUT);
-  Serial.begin(115200);
+  Serial.begin(1000000);
   while ( !Serial ) delay(10);   // for nrf52840 with native usb
 }
 
 void loop() {
   // Get a fresh ADC value
-  double reading = sin(2*M_PI*frequency*millis()/1000);
-  int value = map((reading+1)*127.5, 0, levels, 0, levels);
+  for (int i = 0; i < channels; i++) { 
+    double reading1 = (0.2*sin(2*M_PI*random(5)*frequency*millis()/1000)+0.8*sin(2*M_PI*random(3)*frequency*millis()/1000)+1.2*sin(2*M_PI*random(2)*frequency*millis()/1000))/3;
+    int value1 = map((reading1+1)*127.5, 0, levels, 0, levels);
+    Serial.write(value1);
+  }
 
   // Display the results
-  Serial.write(value);
+  Serial.println();
 
   delay(1000/sampling_frequency);
   blinkLED();
